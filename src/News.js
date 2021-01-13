22c08d84c2e004b86351374f3fe6c8226199e222
Jouni Juntunen <jouni.juntunen@oamk.fi>
2021-01-13 13:24:05 +0200

    Finished example.

---
+++ src/News.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+
+const URL = 'https://newsapi.org/v2';
+const APIKEY = 'd4fd82b33f5a45988cbb0680fdc2b819';
+
+export default class News extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+      selectedItem: null
+    }
+  }
+
+  componentDidMount() {
+    const criteria = 'top-headlines?category=business';
+    const address = URL + '/' + criteria + '&apikey=' + APIKEY;
+    fetch(address)
+      .then(res => res.json())
+      .then ((result) => {
+        console.log('ok');
+        this.setState({
+          error: null,
+          items: result.articles,
+          isLoaded: true
+        })
+      }, (error) => {
+        this.setState({
+          error,
+          items: [],
+          isLoaded: true
+        })
+      })
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+    else if (!isLoaded) {
+      return <p>Loading...</p>;
+    }
+    else {
+      return (
+        <div>
+          {items.map(item => (
+            <div key={item.title}>
+              <h3>{item.title}</h3>
+              <img src={item.urlToImage}></img>
+              <p>{item.description}</p>
+            </div>
+          ))}
+        </div>
+      )
+    }
+  }
+}
